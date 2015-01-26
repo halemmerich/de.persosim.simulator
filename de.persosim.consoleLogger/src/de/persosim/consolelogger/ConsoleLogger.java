9b5d714fbcecf74a8289140879db68c282d85ba0
Martin Boonk <martin.boonk@hjp-consulting.com>
2015-01-26 12:22:53 +0100

    Added a timestamp

--- de.persosim.consoleLogger/src/de/persosim/consolelogger/ConsoleLogger.java
+++ de.persosim.consoleLogger/src/de/persosim/consolelogger/ConsoleLogger.java
@@ -1,14 +1,19 @@
 package de.persosim.consolelogger;
 
+import java.text.DateFormat;
+import java.util.Date;
+
 import org.osgi.service.log.LogEntry;
 import org.osgi.service.log.LogListener;
 
 public class ConsoleLogger implements LogListener {
 
+	DateFormat format = DateFormat.getDateTimeInstance();
+	
 	@Override
 	public void logged(LogEntry entry) {
 		if (entry.getMessage() != null){
-			System.out.println("[" + entry.getBundle().getSymbolicName() + "] " + entry.getMessage());
+			System.out.println("[" + entry.getBundle().getSymbolicName() + " - " + format.format(new Date(entry.getTime())) + "] "  + entry.getMessage());
 		}
 	}
 
